341d1cf23b69a45dae6b1dd2eccdc6e0677c1f72
pyccc <42711563+pyccc@users.noreply.github.com>
2020-02-25 08:43:21 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
-	// add your code here
+    L.mapquest.key = 'YT9hgsgRjsIikBo4lMMBwBRiO3COPaRS';
+
+    // 'map' refers to a <div> element with the ID map
+    var map = L.mapquest.map('map', {
+        center: [32.88, -117.236],
+        layers: L.mapquest.tileLayer('map'),
+        zoom: 14,
+        zoomControl:false
+    });
+
+    L.marker([32.8788,-117.2358]).addTo(map);
 }
